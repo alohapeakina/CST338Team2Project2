5f5fe991e007d909d1d27a108bb9f29895fb38aa
alohapeakina <111400610+alohapeakina@users.noreply.github.com>
2025-08-04 01:05:39 -0700

    Added recipes, shopping list, and weekly plan buttons

--- app/src/main/java/com/example/cst338team2project2/MainActivity.java
+++ app/src/main/java/com/example/cst338team2project2/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.cst338team2project2;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-import com.example.cst338team2project2.databinding.ActivityMainBinding;
 
 /**
  * @author Andrew Lee
@@ -13,11 +11,12 @@ import com.example.cst338team2project2.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-  private ActivityMainBinding binding;
+//  private ActivityMainBinding binding;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
   }
 }
