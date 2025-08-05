318083ab5e866afb8d73a2388bee70c6f06633fb
alohapeakina <111400610+alohapeakina@users.noreply.github.com>
2025-08-04 19:39:08 -0700

    Added getAllUsers, delete, and deleteAll methods

--- app/src/main/java/com/example/pocketmeals/database/UserDAO.java
+++ app/src/main/java/com/example/pocketmeals/database/UserDAO.java
@@ -1,10 +1,13 @@
 package com.example.pocketmeals.database;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 import com.example.pocketmeals.database.entities.User;
+import java.util.List;
 
 /**
  * @author Andrew Lee
@@ -16,5 +19,15 @@ public interface UserDAO {
 
   @Insert(onConflict = OnConflictStrategy.REPLACE)
   void insert(User... user);
+
+  @Delete
+  void delete(User user);
+
+  @Query("SELECT * FROM" + PocketMealsDatabase.USER_TABLE + " ORDER BY username")
+  LiveData<List<User>> getAllUsers();
+
+  @Query("DELETE FROM " + PocketMealsDatabase.USER_TABLE)
+  void deleteAll();
+
 
 }
