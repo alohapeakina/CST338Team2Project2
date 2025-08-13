1fbee5f066c1583c2c13a254eebe3d05a215b9b0
alohapeakina <111400610+alohapeakina@users.noreply.github.com>
2025-08-13 02:56:14 -0700

    Create initial test against User Database table

---
+++ app/src/androidTest/java/com/example/pocketmeals/UserDAOTest.java
@@ -0,0 +1,70 @@
+package com.example.pocketmeals;
+
+import static org.junit.Assert.*;
+
+import android.content.Context;
+import androidx.room.Room;
+import androidx.test.core.app.ApplicationProvider;
+import com.example.pocketmeals.database.PocketMealsDatabase;
+import com.example.pocketmeals.database.dao.UserDAO;
+import com.example.pocketmeals.database.entities.User;
+import java.io.IOException;
+import java.util.List;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Andrew Lee
+ * created: 8/13/2025
+ * Explanation: Database tests for modification of UserDAO within user_table
+ */
+public class UserDAOTest {
+  private UserDAO userDao;
+  private PocketMealsDatabase db;
+
+  @Before
+  public void createDb() throws Exception {
+    Context context = ApplicationProvider.getApplicationContext();
+    db = Room.inMemoryDatabaseBuilder(context, PocketMealsDatabase.class).build();
+    userDao = db.userDAO();
+  }
+
+  @After
+  public void closeDb() throws IOException {
+    db.close();
+  }
+
+  @Test
+  public void insert() {
+    User user = new User("Test User","testpassword");
+    userDao.insert(user);
+
+    List<User> users = userDao.getUserList();
+    assertEquals(1,users.size());
+    assertEquals("Test User", users.get(0).getUsername());
+
+  }
+
+
+  //TODO: Uncomment as testing continues to be configured
+/*  @Test
+  public void delete() {
+  }*/
+
+/*  @Test
+  public void getAllUsers() {
+  }*/
+
+/*  @Test
+  public void deleteAll() {
+  }*/
+
+/*  @Test
+  public void getUserByUserName() {
+  }*/
+
+/*  @Test
+  public void getUserByUserId() {
+  }*/
+}
