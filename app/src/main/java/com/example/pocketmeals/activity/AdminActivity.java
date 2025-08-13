3ae606c051b054e6177672ab11bffed785ebf7e2
alohapeakina <111400610+alohapeakina@users.noreply.github.com>
2025-08-13 03:46:17 -0700

    Added button to remove admin for AdminActivity. Created placeholder toasts

--- app/src/main/java/com/example/pocketmeals/activity/AdminActivity.java
+++ app/src/main/java/com/example/pocketmeals/activity/AdminActivity.java
@@ -1,5 +1,6 @@
 package com.example.pocketmeals.activity;
 import android.os.Bundle;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.pocketmeals.database.PocketMealsRepository;
 import com.example.pocketmeals.databinding.ActivityAdminBinding;
@@ -23,11 +24,19 @@ public class AdminActivity extends AppCompatActivity {
 
     binding.deleteUserButton.setOnClickListener(v -> {
       // TODO: Should send to view that provides text box to select from list of usernames to delete
+      toastMaker("User is deleted");
     });
 
     binding.makeAdminButton.setOnClickListener(view -> {
       //TODO: Should send to view that provides text box to select from a list of usernames to promote
+      toastMaker("Congrats on the promotion");
     });
+
+    binding.removeAdminButton.setOnClickListener(view -> {
+      //TODO: Should send to view that provides text box to select from a list of usernames to promote
+      toastMaker("Returned to normal user");
+    });
+
    }
 
    private void deleteUser(){
@@ -37,6 +46,11 @@ public class AdminActivity extends AppCompatActivity {
    private void makeAdmin(){
     //TODO: Add logic for promoting account to admin
    }
+
+
+  private void toastMaker(String message) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+  }
 
 
 }
