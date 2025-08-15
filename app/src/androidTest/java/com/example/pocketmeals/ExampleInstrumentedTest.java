504946db7d2efa18d985e59441a62f76034a68d8
alohapeakina <111400610+alohapeakina@users.noreply.github.com>
2025-08-15 04:29:55 -0700

    Updated ExampleInstrumentedTest package name so it doesn't fail when running all tests

--- app/src/androidTest/java/com/example/pocketmeals/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/pocketmeals/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
   public void useAppContext() {
     // Context of the app under test.
     Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-    assertEquals("com.example.cst338team2project2", appContext.getPackageName());
+    assertEquals("com.example.pocketmeals", appContext.getPackageName());
   }
 }
