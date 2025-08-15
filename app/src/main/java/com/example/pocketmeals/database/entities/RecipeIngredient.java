aa17bbc3ee547a3345a44b9559d9114dcada3153
alohapeakina <111400610+alohapeakina@users.noreply.github.com>
2025-08-14 23:03:20 -0700

    Added id field to recipeingredients and updated db schema

--- app/src/main/java/com/example/pocketmeals/database/entities/RecipeIngredient.java
+++ app/src/main/java/com/example/pocketmeals/database/entities/RecipeIngredient.java
@@ -3,10 +3,10 @@ package com.example.pocketmeals.database.entities;
 import androidx.room.Entity;
 import androidx.room.ForeignKey;
 
+import androidx.room.PrimaryKey;
 import com.example.pocketmeals.database.PocketMealsDatabase;
 
 @Entity(tableName = PocketMealsDatabase.RECIPE_INGREDIENTS_TABLE,
-        primaryKeys = {"recipeId", "ingredientId"},
         foreignKeys = {
                 @ForeignKey(entity = Recipe.class,
                         parentColumns = "recipeId",    // correct for Recipe
@@ -18,6 +18,8 @@ import com.example.pocketmeals.database.PocketMealsDatabase;
                         onDelete = ForeignKey.CASCADE)
         })
 public class RecipeIngredient {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
     public int recipeId;
     public int ingredientId;
     public double quantity;
